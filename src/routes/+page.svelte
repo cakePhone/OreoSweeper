1234987d7d38b598f9b246c86842be9719185117
cakePhone <ms8042939@gmail.com>
2023-07-19 18:48:31 +0100

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,217 @@
+<script>
+	import { onMount } from 'svelte';
+  import { writable } from 'svelte/store'
+	import Cell from './cell.svelte';
+  
+
+	let game = {
+		size: 15,
+		totalFreeCells: 0,
+		difficulty: 3,
+		showMines: false,
+		field: []
+	};
+
+	let firstClick = true;
+
+	function generateField(game) {
+		let template = [];
+
+		for (let row = 0; row < game.size; row++) {
+			let xrow = [];
+
+			for (let column = 0; column < game.size; column++) {
+				xrow.push({
+					open: false,
+					hasMine: false,
+					nearbyMines: 0,
+					x: row,
+					y: column,
+					showNumber: false,
+					flagged: false
+				});
+			}
+
+			template.push(xrow);
+		}
+
+		return template;
+	}
+
+	function placeMines(clickedX, clickedY) {
+		let totalMines = Math.floor(game.size * game.size * game.difficulty * 0.05);
+
+		for (let minesPlaced = 0; minesPlaced < totalMines; minesPlaced++) {
+			const placeX = Math.floor(Math.random() * game.size);
+			const placeY = Math.floor(Math.random() * game.size);
+
+			const distance = Math.sqrt(Math.pow(clickedX - placeX, 2) + Math.pow(clickedY - placeY, 2));
+
+			const adjCells = adjacentCells(placeX, placeY, false);
+
+			if (!game.field[placeX][placeY].hasMine && distance > Math.sqrt(2)) {
+				game.field[placeX][placeY].hasMine = true;
+
+				for (let i = 0; i < adjCells.length; i++) {
+					adjCells[i].nearbyMines += 1;
+				}
+			} else {
+				minesPlaced -= 1;
+			}
+		}
+	}
+
+	function adjacentCells(x, y, ) {
+		const adjacentCoordinates = [
+			{ x: x - 1, y }, // Left
+			{ x: x + 1, y }, // Right
+			{ x, y: y - 1 }, // Top
+			{ x, y: y + 1 }, // Bottom
+			{ x: x - 1, y: y - 1 }, //Top Left
+			{ x: x - 1, y: y + 1 }, //Bottom Left
+			{ x: x + 1, y: y - 1 }, //Top Right
+			{ x: x + 1, y: y + 1 } //Bottom Right
+		];
+
+		let cells = [];
+
+		for (let i = 0; i < adjacentCoordinates.length; i++) {
+			const adjX = adjacentCoordinates[i].x;
+			const adjY = adjacentCoordinates[i].y;
+
+			if (0 <= adjX && adjX < game.field.length && 0 <= adjY && adjY < game.field.length) {
+				cells.push(game.field[adjX][adjY]);
+			}
+		}
+
+		return cells;
+	}
+
+	function handleCellClick(cell) {
+		if (!cell.open) {
+			if (cell.hasMine) {
+				game.showMines = true;
+				window.alert('You stepped on a heart :c');
+				return;
+			}
+
+			if (firstClick) {
+				placeMines(cell.x, cell.y);
+				firstClick = false;
+			}
+
+			floodFill(cell.x, cell.y);
+			// game.field[cell.x][cell.y].open = true
+		}
+	}
+
+	function floodFill(x, y) {
+		const adjCells = adjacentCells(x, y);
+
+		let freeMines = checkFreeCells();
+
+		if (game.field[x][y].hasMine || game.field[x][y].open || game.field[x][y].nearbyMines > 0) {
+			game.field[x][y].open = true;
+			game.field[x][y].showNumber = true;
+		} else {
+			game.field[x][y].open = true;
+
+			for (let i = 0; i < adjCells.length; i++) {
+				const adjX = adjCells[i].x;
+				const adjY = adjCells[i].y;
+
+				if (!game.field[adjX][adjY].open) {
+					floodFill(adjX, adjY);
+				}
+			}
+		}
+
+		if (freeMines <= 1) {
+			window.alert('You won');
+			game.showMines = true;
+		}
+	}
+
+	function checkFreeCells() {
+		let freeMines = 0;
+
+		for (let row = 0; row < game.size; row++) {
+			for (let collumn = 0; collumn < game.size; collumn++) {
+				if (!game.field[row][collumn].hasMine && !game.field[row][collumn].open) freeMines += 1;
+			}
+		}
+
+		return freeMines;
+	}
+
+	function reset() {
+		window.location.reload();
+	}
+
+	onMount(() => {
+		game.field = generateField(game);
+	});
+
+	$: game.field = firstClick ? generateField(game) : game.field;
+</script>
+
+<div class="flex h-screen w-screen flex-shrink-0 flex-col items-center">
+	<div class="mt-10">
+		{#if firstClick}
+			<label for="size" class="ml-10 w-fit text-2xl text-slate-100">Size of field</label>
+			<input id="size" type="number" bind:value={game.size} max="25" min="10" />
+
+			<label for="difficulty" class="ml-10 w-fit text-2xl text-slate-100">Difficulty</label>
+			<select
+				id="difficulty"
+				on:change={(e) => {
+					game.difficulty = e.target.value;
+				}}
+			>
+				<option value="1">Easy</option>
+				<option selected value="2">Normal</option>
+				<option value="3">Hard</option>
+			</select>
+		{:else}
+			<label for="size" class="ml-10 w-fit text-2xl text-slate-100">Size of field</label>
+			<input id="size" type="number" disabled value={game.size} max="25" min="10" />
+
+			<label for="difficulty" class="ml-10 w-fit text-2xl text-slate-100">Difficulty</label>
+			<select
+				disabled
+				id="difficulty"
+				on:change={(e) => {
+					game.difficulty = e.target.value;
+				}}
+			>
+				<option value="1">Easy</option>
+				<option selected value="2">Normal</option>
+				<option value="3">Hard</option>
+			</select>
+		{/if}
+		<button
+			class="mx-5 rounded-full bg-slate-600 px-6 py-3 text-slate-100 transition-all hover:bg-red-600 hover:text-red-200"
+			on:click={() => reset()}
+			>Reset
+		</button>
+	</div>
+	<div class="mt-10 grid aspect-square h-5/6" id="grid" style="--size: {game.size}">
+		{#each game.field as row}
+			{#each row as cell}
+				<Cell
+					{cell}
+					showMines={game.showMines}
+					on:cell-click={(e) => handleCellClick(e.detail.cell)}
+				/>
+			{/each}
+		{/each}
+	</div>
+</div>
+
+<style>
+	#grid {
+		grid-template-rows: repeat(var(--size), 1fr);
+		grid-template-columns: repeat(var(--size), 1fr);
+		gap: 2px;
+	}
+</style>
---
+++ src/routes/cell.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+  import Icon from '@iconify/svelte';
+
+	const dispatch = createEventDispatcher();
+	export let cell;
+	export let showMines;
+	$: cell;
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class="flex aspect-square items-center justify-center border-4 border-b-slate-700 border-l-slate-500 border-r-slate-700 border-t-slate-500 bg-slate-600"
+	class:open={cell.open}
+	style="--size: {cell.size};"
+	on:click={(e) => {
+		if (!cell.flagged && !showMines) dispatch('cell-click', { cell: cell });
+	}}
+	on:contextmenu={(e) => {
+		if(!cell.open) cell.flagged = !cell.flagged;
+    if(cell.open && cell.flagged) cell.flagged = false
+		e.preventDefault();
+	}}
+>
+	<!-- ({cell.x},{cell.y}) -->
+	{#if cell.showNumber && !cell.flagged}
+		<p
+			class="text-s text-center font-bold"
+			class:text-red-300={cell.nearbyMines >= 3}
+			class:text-orange-300={cell.nearbyMines === 2}
+			class:text-green-300={cell.nearbyMines === 1}
+		>
+			{cell.nearbyMines}
+		</p>
+	{/if}
+
+  {#if cell.flagged && !showMines}
+    <Icon icon="material-symbols:flag" color="white" width="70%" height="70%" />
+  {/if}
+
+  {#if showMines && cell.hasMine}
+    <Icon icon="material-symbols:heart-broken" color="red" width="70%" height="70%"/>
+  {/if}
+</div>
+
+<style lang="postcss">
+	.open {
+		border: none;
+	}
+</style>
